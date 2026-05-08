14b5e5660ae25c23da2174b8425c8c9bfe92804b
A Stranger in the rain <kira020492@gmail.com>
2026-05-08 23:01:03 +0300

    Simplified contact section

--- frontend/src/components/contact/Contact.jsx
+++ frontend/src/components/contact/Contact.jsx
@@ -2,6 +2,7 @@ function Contact() {
     return (
         <section id="contact" className="contact">
             <div className="contact-card">
+
                 <h2 className="section-title">Contact Me</h2>
 
                 <div className="section-divider"></div>
@@ -12,32 +13,33 @@ function Contact() {
                 </p>
 
                 <div className="contact-links">
+
                     <p className="contact-email">
-                        myemail@mail.co
+                        adrianmarcu.tech@gmail.com
                     </p>
 
                     <div className="contact-buttons">
-                        <a href="https://www.linkedin.com/in/adrian-marcu-83b6b7124/"
-                            target="_blank" rel="noopener noreferrer">
-                                LinkedIn
-                            </a>
-                        
-                        <a href="https://github.com/AdrianM-byte"
-                            target="_blank" rel="noopener noreferrer">
-                                GitHub
-                            </a>
+
+                        <a
+                            href="https://www.linkedin.com/in/adrian-marcu-83b6b7124/"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        >
+                            LinkedIn
+                        </a>
+
+                        <a
+                            href="https://github.com/AdrianM-byte"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        >
+                            GitHub
+                        </a>
+
                     </div>
-                    
+
                 </div>
 
-                <form className="contact-form">
-                    <input type="email" placeholder="Your Email"/>
-                    <input type="text" placeholder="Subject"/>
-                    <textarea placeholder="Your message" rows="8"></textarea>
-                    <button type="submit">
-                        Send message
-                    </button>
-                </form>
             </div>
         </section>
     )
